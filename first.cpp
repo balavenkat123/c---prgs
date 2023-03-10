620a9fafea1f5de7e0e92a36d158638994acaf80
balavenkat123 <balavenkat17902@gmail.com>
2023-03-10 20:37:14 +0530

    str

--- first.cpp
+++ first.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main(){
+
     int a,b,c;
     cout<<"enter numbers";
-    cin>> a;
-    cin>>b;
+    cin>> a>>b;
+   
     c=a+b;
     cout<<a<< ends<< b<<ends<<c<<ends<<"bala venkat";
     return 0;
