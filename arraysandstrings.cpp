b2e62c0e1c0b719b630ba87e1da445a402c84971
balavenkat123 <balavenkat17902@gmail.com>
2023-03-12 23:09:17 +0530

    1darr

--- arraysandstrings.cpp
+++ arraysandstrings.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    // 1d array
+    // collection of similar data stored in consequtive memory location
+    int arr[5];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    arr[3]+=18;
+    cout<<arr[3];
+
 }
