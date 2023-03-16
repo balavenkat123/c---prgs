e183b5a56904a762d524fd19c04e41115d3b218e
balavenkat123 <balavenkat17902@gmail.com>
2023-03-16 22:05:20 +0530

    #

--- patterns.cpp
+++ patterns.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+/*
 void pattern3(int n){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-            cout<<j;
+            cout<<i;
         }
         cout<<endl;
     }
@@ -14,5 +14,23 @@ int main(){
     cout<<"enter the number";
     cin>>n;
     pattern3(n);
+    return 0;
+}
+*/
+
+void pattern4(int n){
+    for(int i=n;i<=n;i++){
+        for(int j=n;j>=i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cout<< "enter the number";
+    cin>>n;
+    pattern4(n);
     return 0;
 }
