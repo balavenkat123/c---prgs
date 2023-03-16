0f8a26f9138b42cb710652ca574be5746ce11905
balavenkat123 <balavenkat17902@gmail.com>
2023-03-16 10:30:46 +0530

    pattern2

--- functions.cpp
+++ functions.cpp
@@ -1,13 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-  int i,j;
-  for(i=0;i<=4;i++){
-    for(j=0;j<=4;j++){
+void pattern2(int n){
+
+  for(int i=0;i<=n;i++){
+    for(int j=0;j<i;j++){
       cout<<"*";
     } 
     cout<<endl;
   }
+
+}
+
+int main(){
+  int n;
+  cout<<"enter the number";
+  cin>>n;
+  pattern2(n);
   return 0;
 }
