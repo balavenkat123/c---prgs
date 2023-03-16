394130773b6362cfe8c6799a786f0b6b51964aa3
balavenkat123 <balavenkat17902@gmail.com>
2023-03-16 10:02:51 +0530

    pattern1

--- functions.cpp
+++ functions.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main(){
-    int arr[5];
-   for(int i=0;i<=4;i++){
-    cin>>arr[i];
-
-   }
-    for(int i=0;i<=4;i++){
-      cout<<arr[i]<<endl;
-   
-}
-return 0;
+  int i,j;
+  for(i=0;i<=4;i++){
+    for(j=0;j<=4;j++){
+      cout<<"*";
+    } 
+    cout<<endl;
+  }
+  return 0;
 }
